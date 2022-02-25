39d534f1de603275d574a576bb6acf147b763fa0
Carson Lin <396098651@qq.com>
2022-02-25 15:15:00 +0800

    init
    Signed-off-by: Carson Lin <396098651@qq.com>

---
+++ SyncCodesService/Worker.cs
@@ -0,0 +1,76 @@
+namespace SyncCodesService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly string _workSpace;
+        private FileSystemWatcher _watcher;
+
+        private bool _refreshed = false;
+        public Worker(ILogger<Worker> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _configuration = config;
+            if(string.IsNullOrEmpty(Args.WorkPlace))
+            {
+                _workSpace = config["MyConfig:WorkSpace"];
+            }
+            else
+            {
+                _workSpace = Args.WorkPlace;
+            }
+
+            string codesRoot = Path.Combine(_workSpace, "Codes");
+            _watcher = new FileSystemWatcher(codesRoot);
+            _watcher.IncludeSubdirectories = true;
+            _watcher.EnableRaisingEvents = true;
+            _watcher.Created += OnCreated;
+            _watcher.Deleted += OnDeleted;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _refreshed = false;
+                await Task.Delay(1000, stoppingToken);
+            }
+            _watcher.EnableRaisingEvents = false;
+        }
+
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Refresh(e.FullPath);
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Refresh(e.FullPath);
+        }
+
+        private void Refresh(string path)
+        {
+            if (_refreshed)
+            {
+                return;
+            }
+
+            if (Path.GetExtension(path).ToLower() != ".cs")
+            {
+                return;
+            }
+            string root = _workSpace;
+            if (!Directory.Exists(root))
+            {
+                _logger.LogError($"目录{root}不存在,检查参数");
+            }
+            AdjustTool.Adjust(root + @"\Unity.Model.csproj", "Model");
+            AdjustTool.Adjust(root + @"\Unity.ModelView.csproj", "ModelView");
+            AdjustTool.Adjust(root + @"\Unity.Hotfix.csproj", "Hotfix");
+            AdjustTool.Adjust(root + @"\Unity.HotfixView.csproj", "HotfixView");
+            _refreshed = true;
+        }
+    }
+}
